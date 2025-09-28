215af29556111ea695b9a05a0c10d5d596bc8d3b
suangi-admin <susangi@mylinex.com>
2025-09-29 00:47:16 +0530

    error cofiig

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -1,12 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBxv4lE5iCM-6mSnKaLhrJxYCSXt9thL48",
   authDomain: "insuaranceapp-89261.firebaseapp.com",
@@ -14,12 +12,18 @@ const firebaseConfig = {
   storageBucket: "insuaranceapp-89261.firebasestorage.app",
   messagingSenderId: "243919413656",
   appId: "1:243919413656:web:66309d30ea6b3720a34bed",
-  measurementId: "G-98Q5KM9XWL"
+  measurementId: "G-98Q5KM9XWL",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+
+// ✅ Enable persistence with AsyncStorage
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// Firestore
 const db = getFirestore(app);
 
 export { auth, db };
